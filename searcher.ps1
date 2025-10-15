b87e3c9bb2a68f86016ffd93ad91bf1e81d6e1ea
Chu4hel <106600877+Chu4hel@users.noreply.github.com>
2025-10-15 03:16:31 +0300

    feat: Добавить начальный набор скриптов
    Добавлены основные скрипты, составляющие ядро функциональности:
    
    - b.cmd: Переход в родительскую директорию.
    - env.cmd: Быстрый вызов окна редактирования переменных среды.
    - find-cmd.cmd и searcher.ps1: Поиск команд в системе по части имени.
    - pathc.cmd: Копирование текущего пути в буфер обмена.
    - services.cmd: Открытие оснастки "Службы".

---
+++ searcher.ps1
@@ -0,0 +1,20 @@
+﻿# Этот скрипт принимает один аргумент с командной строки
+param (
+    [string]$SearchTerm
+)
+
+# Проверяем, был ли передан аргумент
+if ([string]::IsNullOrEmpty($SearchTerm)) {
+    Write-Host ''
+    Write-Host '[ Ошибка ] Укажите часть имени команды для поиска.' -ForegroundColor Red
+    Write-Host '          Пример: find-cmd devm'
+    Write-Host ''
+    # Выходим из скрипта
+    return
+}
+
+# Если аргумент есть, выполняем поиск
+Write-Host ''
+Write-Host "[ Поиск команд, содержащих '$SearchTerm' ]" -ForegroundColor Yellow
+Write-Host '---------------------------------------------------------------------'
+Get-Command -Name "*$SearchTerm*" | Select-Object -Property Name, CommandType, Source | Format-Table -AutoSize
